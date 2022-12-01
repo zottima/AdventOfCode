cdbd80427a74aa295ca417936246a99beab0e0cf
zottima <zottelfisch@gmail.com>
2022-12-01 15:42:16 +0100

    First commit

---
+++ src/main/java/AoC2022.java
@@ -0,0 +1,14 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class AoC2022 {
+    public static void main(String[] args) throws Exception {
+        dayOne();
+    }
+
+    public static void dayOne() {
+        final InputStream source = AoC2022.class.getResourceAsStream("day01.txt");
+        Scanner input = new Scanner(source);
+        
+    }
+}
