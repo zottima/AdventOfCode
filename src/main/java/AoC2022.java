98955e9dbc87091042371ad235793349485bbf86
zottima <zottelfisch@gmail.com>
2022-12-01 16:16:17 +0100

    gitignore + readme

--- src/main/java/AoC2022.java
+++ src/main/java/AoC2022.java
@@ -9,6 +9,6 @@ public class AoC2022 {
     public static void dayOne() {
         final InputStream source = AoC2022.class.getResourceAsStream("day01.txt");
         Scanner input = new Scanner(source);
-        
+
     }
 }
