79a48578724e2e93a4574bbef9e2c84b00b9aed3
zottima <zottelfisch@gmail.com>
2022-12-04 13:33:40 +0100

    Day 4, Part 1 and 2 - done

--- src/main/java/AoC2022.java
+++ src/main/java/AoC2022.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
 
 public class AoC2022 {
     public static void main(String[] args) throws Exception {
@@ -13,6 +14,66 @@ public class AoC2022 {
         dayTwo();
         printTitleDay(3);
         dayThree();
+        printTitleDay(4);
+        dayFour();
+    }
+
+    public static void dayFour() {
+        final InputStream source = AoC2022.class.getResourceAsStream("day04.txt");
+
+        // Get assignment pairs
+        int[][] assignmentPairs = new int[1000][4];
+        try(Scanner input = new Scanner(source).useDelimiter(",|-|\\r\\n")) {
+            for (int i=0; input.hasNextLine(); i++) {
+                for (int j=0; j<4; j++) {
+                    assignmentPairs[i][j] = Integer.parseInt(input.next());
+                }
+            }
+        }
+
+        // Check if one range is fully covered by the other range
+        Boolean[] fullyCovered = new Boolean[1000];
+        int numberOfFullyCovered = 0;
+
+        for (int i=0; i<1000; i++) {
+            if (assignmentPairs[i][0] == assignmentPairs[i][2]) {
+                fullyCovered[i] = true;
+                numberOfFullyCovered++;
+            } else if (assignmentPairs[i][0] <= assignmentPairs[i][2]) {
+                if (assignmentPairs[i][1] >= assignmentPairs[i][3]) {
+                    fullyCovered[i] = true;
+                    numberOfFullyCovered++;
+                } else {
+                    fullyCovered[i] = false;
+                }
+            } else if (assignmentPairs[i][1] <= assignmentPairs[i][3]) {
+                fullyCovered[i] = true;
+                numberOfFullyCovered++;
+            } else {
+                fullyCovered[i] = false;
+            }
+        }
+
+        // Check for any overlap
+        Boolean[] hasOverlap = new Boolean[1000];
+        int numberOfOverlappingPairs = 0;
+
+        for (int i=0; i<1000; i++) {
+            if ((assignmentPairs[i][1] < assignmentPairs[i][2]) || assignmentPairs[i][0] > assignmentPairs[i][3]) {
+                hasOverlap[i] = false;
+            } else {
+                hasOverlap[i] = true;
+                numberOfOverlappingPairs++;
+            }
+        }
+
+        // Print solution:
+        System.out.println("Solution Part 1: " + numberOfFullyCovered);
+        System.out.println("Solution Part 2: " + numberOfOverlappingPairs);
+
+        // Print for testing along the way
+//        System.out.println("\nOutput for testing:");
+//        System.out.println(assignmentPairs[4][2]);
     }
 
     public static void dayThree() {
