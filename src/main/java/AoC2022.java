888fad4fc8c47c3098d16c0bffb16bac78293568
zottima <zottelfisch@gmail.com>
2022-12-05 21:04:59 +0100

    Day 5, solved, not done

--- src/main/java/AoC2022.java
+++ src/main/java/AoC2022.java
@@ -1,9 +1,5 @@
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.regex.MatchResult;
+import java.util.*;
 
 public class AoC2022 {
     public static void main(String[] args) throws Exception {
@@ -16,6 +12,115 @@ public class AoC2022 {
         dayThree();
         printTitleDay(4);
         dayFour();
+        printTitleDay(5);
+        dayFive();
+    }
+
+    public static void dayFive() {
+        final  InputStream source = AoC2022.class.getResourceAsStream("day05.txt");
+
+        // Get input
+        char[][] crateInput = new char[8][35];
+        int[] numOfCratesToMove = new int[501];
+        int[] startPosition = new int[501];
+        int[] endPosition = new int[501];
+
+        try(Scanner input = new Scanner(source)) {
+            for (int i=0; (i<8) && input.hasNextLine(); i++) {
+                crateInput[i] = input.nextLine().toCharArray();
+            }
+            // dump the next two lines
+            input.nextLine();
+            input.nextLine();
+
+            for (int j=0; input.hasNextLine(); j++) {
+                input.next();
+                numOfCratesToMove[j] = input.nextInt();
+                input.next();
+                startPosition[j] = input.nextInt();
+                input.next();
+                endPosition[j] =  input.nextInt();
+            }
+        }
+
+        // Put crate stacks into lists
+        ArrayList<String>[] crateStack = new ArrayList[9];
+
+        for (int i = 0; i < 9; i++) {
+            crateStack[i] = new ArrayList<String>();
+        }
+
+        int stackIndex = 0;
+
+        for (int i=1; i<35; i+=4) {
+            Boolean reachedEndofStack = false;
+            for (int j=7; (j>=0) && !reachedEndofStack; j--) {
+                if (Character.isUpperCase(crateInput[j][i])) {
+                    String string = new String();
+                    crateStack[stackIndex].add(string.valueOf(crateInput[j][i]));
+                } else {
+                    reachedEndofStack = true;
+                }
+            }
+            stackIndex++;
+        }
+
+        ArrayList<String>[] copyOfcrateStack = crateStack;
+
+//        // Part 1: Rearrange stacks, moving one crate at a time
+//        for (int i=0; i<501; i++) {     // Loop through instructions
+//            for (int j=0; j<numOfCratesToMove[i]; j++) {    // How many crates to move
+//                int positionInStack = crateStack[startPosition[i]-1].size();    // crate on top of stack
+//                String crateToMove = crateStack[startPosition[i]-1].get(positionInStack-1);     // get crate
+//                crateStack[endPosition[i]-1].add(crateToMove);    // move to new stack
+//                crateStack[startPosition[i]-1].remove(positionInStack-1); // remove stack from where we got it
+//            }
+//        }
+//
+//        // Get all crates on top of each stack
+//        String topCrates = "";
+//        for (int i=0; i<9; i++) {
+//            int topCratePosition = crateStack[i].size()-1;
+//            topCrates+= crateStack[i].get(topCratePosition);
+//        }
+
+        crateStack = copyOfcrateStack;
+
+        // Part 2: Rearrange stacks, moving multiple crates at once
+        for (int i=0; i<501; i++) {     // Loop through instructions
+            List<String> crateListToMove = new ArrayList<String>();
+            for (int j=0; j<numOfCratesToMove[i]; j++) {    // Loop through number of crates to move
+                int indexInStack = crateStack[startPosition[i]-1].size()-1;    // index for crate on top of stack
+                String crateToMove = crateStack[startPosition[i]-1].get(indexInStack);     // get crate
+                crateListToMove.add(crateToMove);
+                crateStack[startPosition[i]-1].remove(indexInStack); // remove stack from where we got it
+            }
+            int loopy = crateListToMove.size();
+            for (int k=0; k<loopy; k++) {
+                int indexInStack = crateListToMove.size()-1;
+                String crateToMove = crateListToMove.get(indexInStack);
+                crateStack[endPosition[i]-1].add(crateToMove);    // move to new stack
+                crateListToMove.remove(indexInStack); // remove stack from where we got it
+            }
+        }
+
+        // Get all crates on top of each stack
+        String topCratesP2 = "";
+        for (int i=0; i<9; i++) {
+            int topCratePosition = crateStack[i].size()-1;
+            topCratesP2+= crateStack[i].get(topCratePosition);
+        }
+
+        // Print for testing along the way
+//        System.out.println("\nOutput for testing:");
+//        System.out.println(crateInput[7][9]);
+//        System.out.println(endPosition[500]);
+//        System.out.println(crateStack[3].get(1));
+//        System.out.println(crateStack[3].size());
+
+        // Print solution:
+       // System.out.println("Solution Part 1: " + topCrates);
+        System.out.println("Solution Part 2: " + topCratesP2);
     }
 
     public static void dayFour() {
