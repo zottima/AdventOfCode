bfa739a5e4b8fe38427d6a7570ae596585ad022d
zottima <zottelfisch@gmail.com>
2023-12-03 17:39:02 +0100

    working solution for example input (d2p1)

--- src/main/java/com/year2023/day02.java
+++ src/main/java/com/year2023/day02.java
@@ -1,12 +1,26 @@
 package com.year2023;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class day02 {
     /*
     Solution approach:
-        -
+        - get input
+        - parse per line:
+            - pairs of number and colour
+            - if number is > than stored number for this colour, replace, otherwise move on
+                (assuming part 2 might need that)
+        - alternative (per line):
+            - check if number is <= cubeLoad for respective colour
+            - if not, set possible games array to 0 and move on to next line
+        - sum up game numbers for possible games (note: game number = index+1)
+
+        - data containers needed:
+            - input array (n)
+            - cube array (n, 3)
+            - possible games array (n)
      */
 
     public static void main(String[] args) throws Exception {
@@ -34,5 +48,50 @@ public class day02 {
                 gameInput[i] = input.nextLine();
             }
         }
+
+        // game setup
+        int red = 12;
+        int green = 13;
+        int blue = 14;
+        boolean[] isGamePossible = new boolean[n];
+        Arrays.fill(isGamePossible, true);
+
+        // check if possible game
+        for (int i=0; i<n; i++) {   // game
+            Scanner game = new Scanner(gameInput[i]);
+            game.next();    // no need for "Game x:" bit
+            game.next();
+            while (game.hasNext() && isGamePossible[i]) {
+                int num = Integer.parseInt(game.next());
+                String cubeColour = game.next();
+                if (cubeColour.contains("red")) {
+                     if (num > red)
+                         isGamePossible[i] = false;
+                }  else if (cubeColour.contains("green")) {
+                    if (num > green)
+                        isGamePossible[i] = false;
+                }   else if (cubeColour.contains("blue")) {
+                    if (num > blue)
+                        isGamePossible[i] = false;
+                }
+            }
+        }
+
+        // sum up IDs of possible games
+        int sum = 0;
+        for (int i=0; i<n; i++) {
+            if (isGamePossible[i]) {
+                int gameId = i +1;
+                sum += gameId;
+            }
+        }
+
+        // Test output
+        System.out.println(isGamePossible[0]);
+        System.out.println(isGamePossible[1]);
+        System.out.println(isGamePossible[2]);
+        System.out.println(isGamePossible[3]);
+        System.out.println(isGamePossible[4]);
+        System.out.println(sum);
     }
 }
